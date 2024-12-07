7d8e7b5a7bb46585edd6ccd4da2055b0526b8e90
Jonathan S <salendahj@gmail.com>
2024-12-07 16:15:12 +0800

    feat: make navbar logo responsive

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light pb-0">
     <div class="container">
         <a class="navbar-brand" href="https://kvsmanado.my.id">
-            <img src="gambar/logoheader.png" alt="KVS Logo" width="180px" style="object-fit: cover;">
+            <img src="gambar/logoheader.png" alt="KVS Logo" class="navbar-logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
